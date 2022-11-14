b48488457f777b622d73ba5f0af8b3eb8e2be77e
metinbeycigim <metinbaydere@gmail.com>
2022-11-13 23:44:54 -0600

    product model built

---
+++ lib/models/product_model.freezed.dart
@@ -0,0 +1,230 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'product_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+ProductModel _$ProductModelFromJson(Map<String, dynamic> json) {
+  return _ProductModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$ProductModel {
+  String get name => throw _privateConstructorUsedError;
+  String get sku => throw _privateConstructorUsedError;
+  String? get location => throw _privateConstructorUsedError;
+  int get quantity => throw _privateConstructorUsedError;
+  String? get cost => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ProductModelCopyWith<ProductModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ProductModelCopyWith<$Res> {
+  factory $ProductModelCopyWith(
+          ProductModel value, $Res Function(ProductModel) then) =
+      _$ProductModelCopyWithImpl<$Res, ProductModel>;
+  @useResult
+  $Res call(
+      {String name, String sku, String? location, int quantity, String? cost});
+}
+
+/// @nodoc
+class _$ProductModelCopyWithImpl<$Res, $Val extends ProductModel>
+    implements $ProductModelCopyWith<$Res> {
+  _$ProductModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? sku = null,
+    Object? location = freezed,
+    Object? quantity = null,
+    Object? cost = freezed,
+  }) {
+    return _then(_value.copyWith(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      sku: null == sku
+          ? _value.sku
+          : sku // ignore: cast_nullable_to_non_nullable
+              as String,
+      location: freezed == location
+          ? _value.location
+          : location // ignore: cast_nullable_to_non_nullable
+              as String?,
+      quantity: null == quantity
+          ? _value.quantity
+          : quantity // ignore: cast_nullable_to_non_nullable
+              as int,
+      cost: freezed == cost
+          ? _value.cost
+          : cost // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_ProductModelCopyWith<$Res>
+    implements $ProductModelCopyWith<$Res> {
+  factory _$$_ProductModelCopyWith(
+          _$_ProductModel value, $Res Function(_$_ProductModel) then) =
+      __$$_ProductModelCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String name, String sku, String? location, int quantity, String? cost});
+}
+
+/// @nodoc
+class __$$_ProductModelCopyWithImpl<$Res>
+    extends _$ProductModelCopyWithImpl<$Res, _$_ProductModel>
+    implements _$$_ProductModelCopyWith<$Res> {
+  __$$_ProductModelCopyWithImpl(
+      _$_ProductModel _value, $Res Function(_$_ProductModel) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? sku = null,
+    Object? location = freezed,
+    Object? quantity = null,
+    Object? cost = freezed,
+  }) {
+    return _then(_$_ProductModel(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      sku: null == sku
+          ? _value.sku
+          : sku // ignore: cast_nullable_to_non_nullable
+              as String,
+      location: freezed == location
+          ? _value.location
+          : location // ignore: cast_nullable_to_non_nullable
+              as String?,
+      quantity: null == quantity
+          ? _value.quantity
+          : quantity // ignore: cast_nullable_to_non_nullable
+              as int,
+      cost: freezed == cost
+          ? _value.cost
+          : cost // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_ProductModel implements _ProductModel {
+  const _$_ProductModel(
+      {required this.name,
+      required this.sku,
+      this.location,
+      required this.quantity,
+      this.cost});
+
+  factory _$_ProductModel.fromJson(Map<String, dynamic> json) =>
+      _$$_ProductModelFromJson(json);
+
+  @override
+  final String name;
+  @override
+  final String sku;
+  @override
+  final String? location;
+  @override
+  final int quantity;
+  @override
+  final String? cost;
+
+  @override
+  String toString() {
+    return 'ProductModel(name: $name, sku: $sku, location: $location, quantity: $quantity, cost: $cost)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_ProductModel &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.sku, sku) || other.sku == sku) &&
+            (identical(other.location, location) ||
+                other.location == location) &&
+            (identical(other.quantity, quantity) ||
+                other.quantity == quantity) &&
+            (identical(other.cost, cost) || other.cost == cost));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, name, sku, location, quantity, cost);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ProductModelCopyWith<_$_ProductModel> get copyWith =>
+      __$$_ProductModelCopyWithImpl<_$_ProductModel>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_ProductModelToJson(
+      this,
+    );
+  }
+}
+
+abstract class _ProductModel implements ProductModel {
+  const factory _ProductModel(
+      {required final String name,
+      required final String sku,
+      final String? location,
+      required final int quantity,
+      final String? cost}) = _$_ProductModel;
+
+  factory _ProductModel.fromJson(Map<String, dynamic> json) =
+      _$_ProductModel.fromJson;
+
+  @override
+  String get name;
+  @override
+  String get sku;
+  @override
+  String? get location;
+  @override
+  int get quantity;
+  @override
+  String? get cost;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ProductModelCopyWith<_$_ProductModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/models/product_model.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ProductModel _$$_ProductModelFromJson(Map<String, dynamic> json) =>
+    _$_ProductModel(
+      name: json['name'] as String,
+      sku: json['sku'] as String,
+      location: json['location'] as String?,
+      quantity: json['quantity'] as int,
+      cost: json['cost'] as String?,
+    );
+
+Map<String, dynamic> _$$_ProductModelToJson(_$_ProductModel instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'sku': instance.sku,
+      'location': instance.location,
+      'quantity': instance.quantity,
+      'cost': instance.cost,
+    };
